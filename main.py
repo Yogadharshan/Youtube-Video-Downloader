2fd27926a9a63633cafce0c9104b937899d1a1a4
yogadharshan <yogadharshan007@gmail.com>
2022-11-20 21:57:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-# importing the module 
-import pytube 
-  
-# where to save 
-SAVE_PATH = "D:/" #to_do 
-  
-# link of the video to be downloaded 
-link="https://www.youtube.com/watch?v=JT80XhYJdBw"
-  
-try: 
-    # object creation using YouTube
-    # which was imported in the beginning 
-    yt = (pytube.YouTube(link)).streams
-except: 
-    print("Connection Error") #to handle exception 
-  
-# filters out all the files with "mp4" extension 
-mp4files = yt.filter(res="720p",mime_type="video/mp4").first() 
-    
-  
-# get the video with the extension and
-# resolution passed in the get() function 
- 
-try: 
-    # downloading the video 
-    mp4files.download(SAVE_PATH)
-    print("Downloading...") 
-except: 
-    print("Some Error!") 
-print('Task Completed!')
+from pytube import YouTube
+
+save_path = "D:/"
+link_to_video = "https://www.youtube.com/watch?v=38f6Vp3fO3o"
+
+def percent(tem, total):
+        perc = (float(tem) / float(total)) * float(100)
+        return perc
+
+def progress_function(stream, chunk,file_handle, bytes_remaining):
+
+    size = stream.filesize
+    p = 0
+    while p <= 100:
+        progress = p
+        print (str(p)+'%')
+        p = percent(bytes_remaining, size)
+        
+        
+def start(url,path_to_save):
+    yt = YouTube(link_to_video, on_progress_callback=progress_function)
+    video1 = yt.streams.filter(progressive = True, file_extension = "mp4").first()
+    try:
+        print("Downloading...")
+        video1.download(save_path)
+        print("Task Completed!")
+    except:
+        print("Some Error!, Try Again")
+        start(url,path_to_save)
+
+start(link_to_video,save_path)
